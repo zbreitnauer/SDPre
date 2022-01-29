b9986e9817274c7f73262ab3298041b1fc8b2c1d
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-01-29 12:36:10 -0700

    Complete Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int first = 1 + (int)(Math.random() *6);
+    int second = 1 + (int)(Math.random() *6);
+    int third = 1 + (int)(Math.random() *6);
+
+    System.out.println(first+second+third);
+
+  }
+}
