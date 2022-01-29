b946558e8c45048302eb166daa3c3f2365b50c85
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-01-29 09:50:53 -0700

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Zachary Breitnauer";
+    int age = 29;
+    String state = "Colorado";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
