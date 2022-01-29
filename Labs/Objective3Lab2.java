aea8c7ecd7df4d74120f80ba425e1985f9ad8049
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-01-29 12:10:09 -0700

    Complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 8;
+    double side2 = 10;
+    double hypotenuse = Math.sqrt(side1*side1+side2*side2);
+
+    System.out.println("The hypotenuse of a triangle with sides 10.0 and 8.0 is " + hypotenuse + ".");
+  }
+}
