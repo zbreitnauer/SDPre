94f5e91b77882b81f2f43ec60cbf644337810ae1
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-01-31 15:59:12 -0700

    Complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 1;
+    int sum = 0;
+
+    while(counter <= 20){
+      sum += counter;
+      counter++;
+}
+System.out.println(sum);
+
+    }
+
+  }
