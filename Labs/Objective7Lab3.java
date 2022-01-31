927f654fcb48f503191f9130e1f6603d52e036ec
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-01-31 15:18:49 -0700

    Complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,16 @@
+public class Objective7Lab3{
+  public static void main(String[] args){
+    int counter = 1;
+
+    while(counter <= 20){
+      System.out.print(counter);
+      counter = counter + 1;
+      if(counter % 2 == 1)
+      System.out.println(" is even.");
+      else
+        System.out.println(" is odd.");
+
+}
+    }
+
+  }
