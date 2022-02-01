d15d2fd23901f0d82b2122c2c615349bb7586dd7
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-01 16:32:34 -0700

    Complete Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3{
+  public static void main(String[] args){
+    int counter = 1;
+
+    for(int i=1; i<=20; i+=1){
+      System.out.print(i);
+      if( i % 2 == 0)
+      System.out.println(" is even.");
+      else
+        System.out.println(" is odd.");
+
+}
+    }
+
+  }
