e44c1bd998ca244b82dca26abb1b69320422691b
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-01 16:07:24 -0700

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    int sum = 0;
+
+    for(int i = 1; i<=20; ++i){
+      sum += i;
+      
+}
+System.out.println(sum);
+
+    }
+
+  }
