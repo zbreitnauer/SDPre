e056f2243b09d6260752cbc17225486ff22a608d
Zachary Breitnauer <zacharybreitnauer@FK8.local>
2022-02-03 11:44:00 -0700

    Complete Objective9Lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,10 @@
+public class Objective9Lab2{
+  public static void main(String[]args){
+    System.out.println("In main");
+    printmessage();
+    System.out.println("In main");
+  }
+    public static void printmessage() {
+      System.out.println("In method");
+    }
+}
